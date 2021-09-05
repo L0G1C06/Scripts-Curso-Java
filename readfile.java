54cbd79b3f3ef61565139afc7b4f5723d53a945b
L0G1C06 <86479444+L0G1C06@users.noreply.github.com>
2021-09-05 16:08:22 -0300

    Create readfile.java

---
+++ readfile.java
@@ -0,0 +1,23 @@
+// lendo um arquivo .txt
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("filename.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ocorreu um erro");
+            e.printStackTrace();
+        }
+    }
+}
